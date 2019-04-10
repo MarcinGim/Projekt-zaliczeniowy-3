566cedde98d4deb87a6d1b2d02fb79bb915a1ee4
Marcin Bieniek <marcingim@gmail.com>
2019-04-10 12:54:18 +0200

    Add mobile settings

---
+++ js/script.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var hamburger = document.getElementById('hamburger');
+
+var leftNavbar = document.getElementById('leftnav');
+
+hamburger.addEventListener('click', function(){leftNavbar.classList.toggle('left-nav-backgr');
+});
+
+
+
+
+
+
+
---
+++ left-menu.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var hamburger = document.getElementById('hamburger');
+
+var leftnav = document.getElementById('leftnav');
+
+hamburger.addEventListener('click', function(){leftnav.classList.toggle('left-nav-width');
+});
+
+
+
+
+
+
+
