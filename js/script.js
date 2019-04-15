63b438abe49e4103800223de80cd3061ba794518
Marcin Bieniek <marcingim@gmail.com>
2019-04-15 21:41:50 +0200

    Add JS file

--- js/script.js
+++ js/script.js
@@ -1,15 +1,84 @@
-'use strict'
+	'use strict'
 
-var hamburger = document.getElementById('hamburger');
+// Close modal js
 
-var leftNavbar = document.getElementById('leftnav');
+		var modal = document.getElementById('myModal');
 
-hamburger.addEventListener('click', function(){leftNavbar.classList.toggle('left-nav-backgr');
-});
+		var btn = document.getElementById("myBtn");
+
+		var span = document.getElementsByClassName("close")[0];
+
+		btn.onclick = function() {
+	  	modal.style.display = "block";
+		}	
+
+		span.onclick = function() {
+	  	modal.style.display = "none";
+		}	
 
 
+		window.onclick = function(event) {
+	  	if (event.target == modal) {
+	    modal.style.display = "none";
+	  		}
+		}
+
+// Login modal
+
+		var modal2 = document.getElementById('myModal2');
+
+		var btn2 = document.getElementById("loginBtn");
+
+		var span2 = document.getElementsByClassName("close2")[0];
+
+		btn2.onclick = function() {
+	  	modal2.style.display = "block";
+		}	
+
+		span2.onclick = function() {
+	  	modal2.style.display = "none";
+		}	
 
 
+		window.onclick = function(event) {
+	  	if (event.target == modal2) {
+	    modal2.style.display = "none";
+	  		}
+		}
+
+// Left menu open/close
+
+		var hamburger = document.getElementById('hamburger');
+
+		var leftNavbar = document.getElementById('leftnav');
+
+		var container = document.getElementById('bodyGeneral');	
+
+		hamburger.addEventListener('click', function(){	leftNavbar.classList.toggle('left-nav-width');
+		});
+		
+		hamburger.addEventListener('click', function(){	container.classList.toggle('body-when-mobile-menu-open');
+		});		
+
+// logout button on mobile
+
+		var modal = document.getElementById('myModal');
+
+		var btn = document.getElementById("myBtnMobile");
+
+		var span = document.getElementsByClassName("close")[0];
+
+		btn.onclick = function() {
+	  	modal.style.display = "block";
+		}	
+
+		span.onclick = function() {
+	  	modal.style.display = "none";
+		}	
 
 
-
+		window.onclick = function(event) {
+	  	if (event.target == modal) {
+	    modal.style.display = "none";
+	  		}
+		}
