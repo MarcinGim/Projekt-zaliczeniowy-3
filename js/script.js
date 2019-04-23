ca7452d1ee2dea844fde6d1985d770a5bfd8db19
Marcin Bieniek <marcingim@gmail.com>
2019-04-23 09:52:26 +0200

    Correct js code

--- js/script.js
+++ js/script.js
@@ -2,83 +2,69 @@
 
 // Close modal js
 
-		var modal = document.getElementById('myModal');
+var modal = document.getElementById('myModal');
 
-		var btn = document.getElementById("myBtn");
+var btn = document.getElementById("myBtn");
 
-		var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[0];
 
-		btn.onclick = function() {
-	  	modal.style.display = "block";
-		}	
+btn.onclick = function() {
+	modal.style.display = "block";
+}	
 
-		span.onclick = function() {
-	  	modal.style.display = "none";
-		}	
+span.onclick = function() {
+	modal.style.display = "none";
+}	
 
-
-		window.onclick = function(event) {
-	  	if (event.target == modal) {
+window.onclick = function(event) {
+	if (event.target == modal) {
 	    modal.style.display = "none";
 	  		}
 		}
 
 // Login modal
 
-		var modal2 = document.getElementById('myModal2');
+var modal2 = document.getElementById('myModal2');
 
-		var btn2 = document.getElementById("loginBtn");
+var btn2 = document.getElementById("loginBtn");
 
-		var span2 = document.getElementsByClassName("close2")[0];
+var span2 = document.getElementsByClassName("close2")[0];
 
-		btn2.onclick = function() {
-	  	modal2.style.display = "block";
-		}	
+btn2.onclick = function() {
+	modal2.style.display = "block";
+}	
 
-		span2.onclick = function() {
-	  	modal2.style.display = "none";
-		}	
+span2.onclick = function() {
+	modal2.style.display = "none";
+}	
 
-
-		window.onclick = function(event) {
-	  	if (event.target == modal2) {
-	    modal2.style.display = "none";
-	  		}
-		}
+window.onclick = function(event) {
+	if (event.target == modal2) {
+	   		modal2.style.display = "none";
+	  	}
+	}
 
 // Left menu open/close
 
-		var hamburger = document.getElementById('hamburger');
+var hamburger = document.getElementById('hamburger');
 
-		var leftNavbar = document.getElementById('leftnav');
+var leftNavbar = document.getElementById('leftnav');
 
-		var container = document.getElementById('bodyGeneral');	
+var container = document.getElementById('bodyGeneral');	
 
-		hamburger.addEventListener('click', function(){	leftNavbar.classList.toggle('left-nav-width');
-		});
+hamburger.addEventListener('click', function(){	
+	leftNavbar.classList.toggle('left-nav-width');
+});
 		
-		hamburger.addEventListener('click', function(){	container.classList.toggle('body-when-mobile-menu-open');
-		});		
+hamburger.addEventListener('click', function(){	
+	container.classList.toggle('body-when-mobile-menu-open');
+});		
 
 // logout button on mobile
 
-		var modal = document.getElementById('myModal');
+var btnMobile = document.getElementById("myBtnMobile");
 
-		var btn = document.getElementById("myBtnMobile");
+btnMobile.onclick = function() {
+	modal.style.display = "block";
+}	
 
-		var span = document.getElementsByClassName("close")[0];
-
-		btn.onclick = function() {
-	  	modal.style.display = "block";
-		}	
-
-		span.onclick = function() {
-	  	modal.style.display = "none";
-		}	
-
-
-		window.onclick = function(event) {
-	  	if (event.target == modal) {
-	    modal.style.display = "none";
-	  		}
-		}
